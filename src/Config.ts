7c126873af7fff5ea8217921278c64d9f0bd66f7
mtrifonov <mtrifonov.design@gmail.com>
2025-05-18 22:49:51 +0200

    Ready to deploy

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,4 @@
-const production = false;
+const production = true;
 const CorePluginBase = production ? "https://mtrifonov-design.github.io/pinsandcurves-panels/#" : "http://localhost:5174/#";
 const CoreBackgroundBase = production ? "https://mtrifonov-design.github.io/pinsandcurves-background-services/" : "http://localhost:8000/";
 
