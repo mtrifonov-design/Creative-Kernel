18d264ce9778d6a4c8da56acc45d5b7ea55274ab
mtrifonov <mtrifonov.design@gmail.com>
2025-07-05 22:12:14 +0200

    changed cyb spag template to be more performant

--- src/Persistence_Templates/index.ts
+++ src/Persistence_Templates/index.ts
@@ -1,6 +1,6 @@
 import bb from './BouncingBall.json';
 import s from './ForSasha.json'
-import cs from './cyber_spag_test2.json'
+import cs from './cybspagh_renewedtemplate.json'
 import ll from './LaserLinguine.json'
 import liquidl from './newlissajoustemplate.json'
 
