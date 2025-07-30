1585cd0651628f9eac56e44e8ad32b49e34a64b9
mtrifonov <mtrifonov.design@gmail.com>
2025-07-30 19:05:21 +0200

    featuring new star shaped domain tool

--- src/Sidebar/ContextMenus/Tools.tsx
+++ src/Sidebar/ContextMenus/Tools.tsx
@@ -85,7 +85,9 @@ function ToolContextMenu(p: {}) {
         <Tool label={"Cyber Spaghetti Controls"} icon={"dinner_dining"} address={base+"cyberspaghetti-controlconsole"} />
         <Tool label={"Liquid Lissajous Viewer"} icon={"dinner_dining"} address={base+"liquidlissajous"} />
         <Tool label={"Liquid Lissajous Controls"} icon={"dinner_dining"} address={base+"liquidlissajous-controlconsole"} />
-        
+        <Tool label={"Star Shaped Domain Wipe Viewer"} icon={"dinner_dining"} address={base+"starshapeddomainwipe"} />
+        <Tool label={"Star Shaped Domain Wipe Controls"} icon={"dinner_dining"} address={base+"starshapeddomainwipe-controlconsole"} />
+
     </div>
 }
 
