27dee332fdb277d4612e66d4e3f0c75f78dab395
mtrifonov <mtrifonov.design@gmail.com>
2025-08-26 12:30:34 +0200

    tiny visual padding fix, long overdue

--- src/Sidebar/index.tsx
+++ src/Sidebar/index.tsx
@@ -119,8 +119,8 @@ function Sidebar() {
         <div style={{
             width: "100%",
             height: "100%",
-            padding: "2px",
-            paddingRight: "4px",
+            padding: "0px",
+            paddingRight: "6px",
             boxSizing: "border-box",
         }}>
             <div style={{
