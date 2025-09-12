a3da076ae07feb9100bccb8201d2899f31e01ae5
mtrifonov <mtrifonov.design@gmail.com>
2025-09-12 19:06:49 +0200

    added neonshower template

--- src/Persistence_Templates/index.ts
+++ src/Persistence_Templates/index.ts
@@ -4,6 +4,7 @@ import cs from './cybspagh_renewedtemplate.json'
 import ll from './LaserLinguine.json'
 import liquidl from './liquidlissajousv05.json'
 import echoknight from './echoknight_t.json'
+import neonshower from './neonshowertemplate.json'
 
 const TEMPLATES = {
     "default": JSON.stringify(bb),
@@ -12,6 +13,7 @@ const TEMPLATES = {
     "laserlinguine": JSON.stringify(ll),
     "liquidlissajous": JSON.stringify(liquidl),
     "echoknight": JSON.stringify(echoknight),
+    "neonshower": JSON.stringify(neonshower),
 }
 
 export { TEMPLATES }; 
